10499b3a2f682250ec4cf9a637c168957bbfb3d5
Sébastien Lavoie <lavoiesl@users.noreply.github.com>
2018-07-17 13:01:17 -0400

    Add repo and title upon notification
    Subtitle will be "Last: org/repo" in case of multiple notifications

--- octobox_bitbar.rb
+++ octobox_bitbar.rb
@@ -1,9 +1,12 @@
 #!/usr/ruby
 
+# https://github.com/lavoiesl/dot-files/blob/master/octobox_bitbar.rb
+
 IMAGE = "iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAQAAADZc7J/AAAAAmJLR0QA/4ePzL8AAAAJcEhZcwAAFiUAABYlAUlSJPAAAAAHdElNRQfiAhcSADAZLWvdAAADJ0lEQVRIx53VbWiWZRQH8N993ZvuxaXk03oxCC1IYmUQFCWNMpoibrXsS0Fo0LfSJZK9fMkPRrCCyE+R0IogejHrS30IC0RaZi82goVJGi6j2qbW5l6e7dnTh/t+7t17IUfn4oHrOfc5/+ucc53/uWLT0uo1NxrVl2lqbHO7Y0qZZq3tnjOit6KIcgDvuxqRki1+VWWFfWpR9JjTpqzUJVYWOWfjfAA9RtNd2ZBLxNnJsZJ/NGTWVW6rfKvKAcQCyiIsQ1nIAEOqSSCCMB/AKVdaiETOm2AuQDE78WIAI/mwK/Kw+xEtaC31l59mF7Fbfe6Mskg53c0Xw5SbpeVI5E6NYrFYyP3yuzi3C+rdk4+g1W61ab1rfOErQ+oUFDRgyKABFzS4w93GUp9Re3yUAKzzRtYBYx71w38UcI2u9CgW2epwQItxQRBU2566H9RjWea23I8OqUaPp9Sk1pMeSmpQlSqC8w6DV61U5+UMYLcaKzwPPndSLAhi1QlAlJanyqHU4XoErRnAekGkKbuvkBY3JABBJKT3m8iUSDTdbcZFIpPpv6WZfXaNwZRYZL128KVFYvsygHcFVY6ArR4QxMpJBEnGfe7VZkCfP33qKuz0gurcXNhjJ67xmT/0GXSfTfq8mXze64wNWOJD/X7zt455WBHbZcgZA/arR5vfvVUpYmI+bLMOsRHPOGHVDPfr/GKHYTzuQRdS1oRpgAon3tHkmMWWOOJASrXIAd1q1eixxnsZH4JoLgCDNuowIdLspEdscVqzSMk2GwzkogqiZB5EMwYbvG2/g1artxclkZ/dpTiLkbk+iOdA1GpMGyxZhYwB5k9hpjzrhEaL9WvX7pxFLnfK07OsssRfN2RTpl7lqDHDzurMdC86a9i4b12b6TYb8kElgukqPOm4G0zqUbArF9Flek1YrdcTWQrR7BQKvteppKjFWlMzwi25VYsxZa/4zvKUgtF0EUs69GsyodOluucdJt0KXjLhJgN25LnQpWjSpDHfqLvoUK/1tTGTSoo+TvqglHHyCscX9C6E9LVKCb9O+X+u5spUvkXbAl+laZnyiaP8C2RC6xkl3ixRAAAAAElFTkSuQmCC"
 
-require 'net/http'
+require 'date'
 require 'json'
+require 'net/http'
 require 'open3'
 
 TMP_DATA_FILE = '/tmp/octobox-bitbar-ids.json'.freeze
@@ -57,14 +60,37 @@ File.write(TMP_DATA_FILE, JSON.generate(current_ids))
 if current_ids.empty?
   TerminalNotifier.remove(:octobox)
 elsif notify_ids.nil? || !notify_ids.empty?
-  text = pluralize(current_ids.size, "unread item")
+  notification = data['notifications']
+    .select {|n| notify_ids.include?(n['github_id']) }
+    .sort_by { |n| DateTime.parse(n["updated_at"]) }
+    .last
+
+  if notification['repo']
+    repo = notification['repo']['repo_url'].match(%r(([^/]+/[^/]+)$))[1]
+  end
+
+  time = DateTime.parse(notification["updated_at"])
+  if time.to_date == Time.now.to_date
+    time = time.strftime("%H:%M:%S")
+  else
+    time = time.strftime("%Y-%m-%d")
+  end
+  subtitle = "#{repo} - #{notification['subject']['type']} - #{time}"
+  text = notification['subject']['title']
+
+  if notify_ids.size == 1
+    link = notification['web_url']
+  else
+    subtitle = "Last: #{subtitle}"
+    link = "https://octobox.shopify.io/"
+  end
 
   TerminalNotifier.notify(
     text,
-    title: 'Ocotobox',
-    subtitle: 'Pending review',
+    title: "Octobox – #{pluralize(current_ids.size, "unread item")}",
+    subtitle: subtitle,
     group: :octobox,
-    execute: 'open https://octobox.shopify.io/',
+    execute: "open '#{link}'",
     appIcon: "data:image/png;base64,#{IMAGE}",
   )
 end
