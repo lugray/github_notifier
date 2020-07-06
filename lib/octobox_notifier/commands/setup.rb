e79263cdcfe4cf0bdfb2f52905d80884d762549c
Lisa Ugray <lisa.ugray@shopify.com>
2020-07-06 09:21:04 -0400

    Settings

--- lib/octobox_notifier/commands/setup.rb
+++ lib/octobox_notifier/commands/setup.rb
@@ -6,17 +6,12 @@ module OctoboxNotifier
     class Setup < OctoboxNotifier::Command
       def call(args, _name)
         options = parse_args(args)
-        unless options.key?(:host)
-          puts options.key?(:host).inspect
-          options[:host] = CLI::UI::Prompt.ask("What host would you like to use?", default: "octobox.io")
-        end
         unless options.key?(:token)
-          options[:token] = CLI::UI::Prompt.ask("What is your octobox token? (Found at https://#{options[:host]}/settings)")
+          options[:token] = CLI::UI::Prompt.ask("What is your GitHub API token? (Create at https://github.com/settings/tokens/new?scopes=notifications&description=notifier)")
         end
         unless options.key?(:notify)
           options[:notify] = CLI::UI::Prompt.ask("Would you like to get system notifications?", options: ["Yes", "No"]) == "Yes"
         end
-        OctoboxNotifier::Config.set("server", "host", options[:host])
         OctoboxNotifier::Config.set("server", "token", options[:token])
         OctoboxNotifier::Config.set("notification", "display", options[:notify])
       end
@@ -26,11 +21,7 @@ module OctoboxNotifier
         OptionParser.new do |opts|
           opts.banner = "Usage: octobox_notifier setup [options]"
 
-          opts.on("-h", "--host <HOST>", "The octobox host") do |host|
-            options[:host] = host
-          end
-
-          opts.on("-t", "--token <TOKEN>", "Your octobox API token (found in settings)") do |token|
+          opts.on("-t", "--token <TOKEN>", "Your GitHub API token (Create at https://github.com/settings/tokens/new?scopes=notifications&description=notifier)") do |token|
             options[:token] = token
           end
 
